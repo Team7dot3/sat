1b8a219d75b098e3f3f113f149bcbf7823b651b9
Andrew Hendrix <andrewthendrix@gmail.com>
2015-10-28 15:32:07 -0600

    Moved Malloc include to sat_types.h

--- src/sat_types.h
+++ src/sat_types.h
@@ -14,6 +14,7 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <malloc.h>
 
 /*******************************************************************************************
  * Color codes for printing to stdout.
