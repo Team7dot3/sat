3c4106e4d4fe22f48dc98e61304e48f5ef74f377
TJHooker33 <rolyat.wilson@gmail.com>
2015-11-01 19:52:25 -0700

    Updated gitignore to ignore dSYM files in /bin/. Also added Logger to diff_test

--- test/diff_test.py
+++ test/diff_test.py
@@ -12,44 +12,95 @@ def exec_process(args):
   ps         = Popen(args, stdout=PIPE)
   (out, err) = ps.communicate()
   exit_code  = ps.wait()
-  print out
+  # print out
   return out
 
 def init_random():
   NOW = time.time()
   random.seed(NOW)
 
-class Minisat():
+class Logger:
+  def __init__(self, path):
+    self.log_file = open(path, 'w+')
+
+  def write(self, str):
+    self.log_file.write('==============================\n\n')
+    self.log_file.write(str)
+    self.log_file.write('\n==============================\n')
+    
+  def close(self):
+    self.log_file.close()
+
+class Minisat:
   def __init__(self, path):
     self.path = path
 
   def run(self):
-    exec_process(['minisat', self.path])
+    return exec_process(['minisat', self.path])
+
+class Sat:
+  def __init__(self, path):
+    self.path = path
+
+  def run(self):
+    return exec_process(['../bin/sat_solver.o', self.path])
 
 class RandomTesting:
-  def __init__(self, path, max):
+  def __init__(self, path, max_num, itrs):
     self.path = path
-    self.max = max
+    self.max  = max_num
+    self.itrs = itrs
 
   def gen_rand_input(self):
-    self.nbvars = random.randint(1, self.max)
+    self.nbvars    = random.randint(1, self.max)
     self.nbclauses = random.randint(1, self.max)
+
     with open(self.path, 'wb') as f:
       f.write('p cnf ' + str(self.nbvars) + ' ' + str(self.nbclauses) + ' \n')
+      
       for i in range(0, self.nbclauses):
-        for j in range(0, random.randint(1, self.max)):
+        for j in range(0, random.randint(1, self.nbvars)):
           if random.randint(0, MAX_INT) % 2 == 0:
             f.write('-')
-          b = random.randint(1, self.max)
+          b = random.randint(1, self.nbvars)
           f.write(str(b) + ' ')
         f.write('0 \n')
 
   def run(self):
-    Minisat(self.path).run()
+    log = Logger('../txt/log_diff_test.txt')
+    passed = 0
+    
+    for i in range(0, self.itrs):
+      prev_pass = passed
+      passed += self.test()
+      if prev_pass == passed:
+        input_file = None
+        with open(self.path, 'r') as f:
+          input_file = f.read()
+        log.write('Test FAILED with input:\n' + input_file)
+
+    log.close()
+    return passed
+
+  def test(self):
+    ms_out    = Minisat(self.path).run().split('\n')
+    ms_result = ms_out[len(ms_out) - 2] + '\n'
+    t7_out    = Sat(self.path).run()
+    
+    if ms_result == t7_out:
+      return 1
+    else:
+      return 0
 
 if __name__ == '__main__':
   init_random()
-  rt = RandomTesting('../txt/diff_test.txt', 10)
+  max_var = 5
+  test_runs = 10
+  rt = RandomTesting('../txt/diff_test.txt', max_var, test_runs)
   rt.gen_rand_input()
-  rt.run()
+  passed = rt.run()
+  print 'Differential Testing:'
+  print '  PASSES: ' + str(passed)
+  print '  FAILED: ' + str(test_runs - passed)
+
 
