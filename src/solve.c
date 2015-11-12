f8724cd2706bf7572a59e7e105861fc3681b6061
Andrew Hendrix <andrewthendrix@gmail.com>
2015-11-12 13:05:54 -0700

    added new algorithm to solve.c

--- src/solve.c
+++ src/solve.c
@@ -28,7 +28,19 @@
 int solve(UNMOLESTED_INPUT *unin, MOLESTED_INPUT *in)
 {
   LOG("solve CALLED", 2);
-  int toReturn = process_and(unin, in);
+  int nbvars = unin->nbvars;
+  //int toReturn = process_and(unin, in);
+  int * counts = (int*)malloc(sizeof(int)* nbvars);
+  int * setvals = (int*)malloc(sizeof(int)* nbvars);
+  int i;
+  for (i = 0; i < nbvars; i++)
+  {
+    counts[i] = 0;
+    setvals[i] = -1;
+  }
+  int toReturn = solver(unin->data, nbvars, unin->nbclauses, unin->clause_lengths, counts, setvals, 0);
+  free(counts);
+  free(setvals);
   LOG("solve RETURNING", 2);
   return toReturn;
 }
@@ -158,4 +170,92 @@ void make_val(int* vals, int input, int num_vals)
   }
 }
 
+int solver(int ** values, int val_count, int num_clauses, int * clause_length, int * counts, int * setvals, int pos)
+{
+  if (pos >= val_count)
+  {
+    return 0;
+  }
 
+  int sat_level = 0;
+  int i;
+  for (i = 0; i < num_clauses; i++)
+  {
+    int clause_result = process_clause(values[i], val_count, clause_length[i], setvals);
+      switch (clause_result)
+      {
+      case -1:
+        return 0;
+      case 1:
+        sat_level++;
+        break;
+      default:
+        break;
+      }
+  }
+  if (sat_level == num_clauses)
+  {
+    return 1;
+  }
+  if (counts[pos] < 0)
+  {
+    setvals[pos] = 0;
+  }
+  else
+  {
+    setvals[pos] = 1;
+  }
+  if (solver(values, val_count, num_clauses, clause_length, counts, setvals, pos + 1))
+  {
+    return 1;
+  }
+  else
+  {
+    setvals[pos] = !setvals[pos];
+
+    return solver(values, val_count, num_clauses, clause_length, counts, setvals, pos + 1);
+  }
+}
+
+int process_clause(int* values, int val_count, int clause_length, int * setvals)
+{
+  int setcount = 0;
+  int i;
+  for (i = 0; i < clause_length; i++)
+  {
+    int val;
+    int variable = values[i];
+    if (variable < 0)
+    {
+      val = setvals[(-1 * variable) - 1];
+      if (val == 0)
+      {
+        val = 1;
+      }
+      else if (val == 1)
+      {
+        val = 0;
+      }
+    }
+    else
+    {
+      val = setvals[variable - 1];
+    }
+
+    switch (val)
+    {
+    case 1:
+      return 1;
+    case 0:
+      setcount++;
+      break;
+    case -1:
+      break;
+    }
+  }
+  if (setcount == clause_length)
+  {
+    return -1;
+  }
+  return 0;
+}
