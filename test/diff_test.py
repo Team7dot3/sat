865da597d63eba713255bde5f271b5f3deff2dd5
TJHooker33 <rolyat.wilson@gmail.com>
2015-11-16 01:19:15 -0700

    Adding readme for tests.

--- test/diff_test.py
+++ test/diff_test.py
@@ -16,7 +16,7 @@ LOG_PATH   = 'txt/log_diff_test.txt'
 EXE_PATH   = 'bin/sat_solver.o'
 MINI_PATH  = 'minisat'
 MAX_VARS   = 40 # The maximum number of variables and clauses
-TEST_RUNS  = 1000 # The number of tests to run
+TEST_RUNS  = 10000 # The number of tests to run
 
 def exec_process(args):
   """
