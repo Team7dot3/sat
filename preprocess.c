1f9cac6597ce0ac8d0e01709267c90404012272c
Andrew Hendrix <andrewthendrix@gmail.com>
2015-11-19 15:48:56 -0700

    added some stubs and code beginnings stuff to preprocess

--- preprocess.c
+++ preprocess.c
@@ -32,8 +32,62 @@ int preprocess(INPUT *in)
   int nbvars = in->nbvars;
   int pos_val_sums = in->pos_val_sums;
   int neg_val_sums = in->neg_val_sums;
+  //Preprocessing types
+  //
+  //Preprocessing Unit propigation
+  //
+  //Preprocessing Pure Literals
+  //If a variable exists as only positive, or only negative, all rows it exists on can be removed. (They can always evaluate to true from that one variable)
+  //
+  //Preprocessing Pure Clauses
+  //If a clause contains both positive and negative values for a variable, remove the clause.
+  //
+  //Increase chance of contradictions early on.
+  //Reorder rows from smallest to largest
+  //
+  //Rename variables
+  //Rename the variables in the order you find them after reordering
+  //Remove all variables that don't appear (decrement the counter)
 
 
 
+}
+
+
+/*******************************************************************************************
+* NAME :             unit_propigation
+*
+* DESCRIPTION :        If there is a unit clause (size 1),
+*                      propigate it by checking if there is an opposing unit clause (instant contradiction),
+*                      assigning it (to true if positive, or false if negative),
+*                      remove it everywhere it appears by removing the line if it evaluates to true on a given line, or removing it from the line if it evaluates to false
+*                      Keep all numbers correct (removing a variable will decrement several values elsewhere, and removing a line will decrement several variables elsewhere.
+*
+* INPUTS :
+*      PARAMETERS :
+*          INPUT  *in   input
+*
+* OUTPUTS :
+*      RETURN :
+*          int                       1 on unit propigated, 0 on nothing propigated, -1 on error
+*/
+int unit_propigation(INPUT *in)
+{
+  int i;
+  int toReturn = 0;
+  for (i = 0; i < in->nbclauses; i++)
+  {
+    if (clause_lengths[i] == 1)
+    {
+      toReturn = 1;
+
+      break;
+    }
+  }
+  return toReturn;
+}
+
+int set_variable(INPUT *in, int variable, int set)
+{
 
 }
