05580c3855bf38bbfed8d58a9e054664d0b59539
TJHooker33 <rolyat.wilson@gmail.com>
2015-11-21 15:05:21 -0700

    removing compiled python files from repo, pure clauses kept to minimum during differential testing

--- test/diff_test.py
+++ test/diff_test.py
@@ -12,13 +12,12 @@ from lib.sat_solver.logger import *
 from lib.sat_solver.sat import *
 import time
 
-
 INPUT_PATH  = 'txt/diff_test.txt'
-LOG_PATH    = 'txt/log_diff_test.txt'
-EXE_PATH    = 'bin/sat_solver.o'
-MINI_PATH   = 'minisat'
-MAX_VARS    = 10 # The maximum number of variables
-MAX_CLAUSES = 10 # The maximum number of clauses
+LOG_PASS    = 'txt/log_diff_pass.txt'
+LOG_FAIL    = 'txt/log_diff_fail.txt'
+MAX_VARS    = 20 # The maximum number of variables
+MAX_CLAUSES = 1000 # The maximum number of clauses
+MAX_PURES   = 0.20 # The maximum ration of pure clauses to clauses
 TEST_RUNS   = 10 # The number of tests to run
 
 def init_random():
@@ -27,69 +26,6 @@ def init_random():
   """
   random.seed(time.time())
 
-# class Logger:
-#   """
-#   Logger is a wrapper for a file writer. 
-#   """
-
-#   def __init__(self, path):
-#     """
-#     Initializes the Logger with the input path.
-#     The input path represents the file to write log messages to.
-#     """
-#     self.log_file = open(path, 'wb')
-
-#   def write(self, str):
-#     """
-#     Writes the input string to the log file.
-#     """
-#     self.log_file.write('==============================\n\n')
-#     self.log_file.write(str)
-#     self.log_file.write('\n==============================\n')
-    
-#   def close(self):
-#     """
-#     Closes the log file. Do not call write() after calling close()!
-#     """
-#     self.log_file.close()
-
-
-# class Minisat:
-#   """
-#   Provides an easy mechanism for calling minisat with an input file.
-#   """
-
-#   def __init__(self, path):
-#     """
-#     Sets the input file to be passed to minisat.
-#     """
-#     self.path = path
-
-#   def run(self):
-#     """
-#     Returns the output of minisat.
-#     """
-#     return Process().run([MINI_PATH, self.path])
-
-
-# class Sat:
-#   """
-#   Provides an easy machnism for call sat_solve.o with an input file.
-#   """
-
-#   def __init__(self, path):
-#     """
-#     Sets the input file to be passed to sat_solver.o.
-#     """
-#     self.path = path
-
-#   def run(self):
-#     """
-#     Returns the output of sat_solver.o.
-#     """
-#     return Process().run([EXE_PATH, self.path])
-
-
 class RandomTesting:
   """
   A random tester for sat_solver.o. This creates a CNF input with a random
@@ -97,7 +33,7 @@ class RandomTesting:
   minisat. If the outputs do not match, the test fails.
   """
 
-  def __init__(self, path, max_vars, max_clauses, itrs):
+  def __init__(self, path, max_vars, max_clauses, max_pures, itrs):
     """
     Initializes the random testing.
 
@@ -109,7 +45,12 @@ class RandomTesting:
     self.path        = path
     self.max_vars    = max_vars
     self.max_clauses = max_clauses
+    self.max_pures   = max_clauses * max_pures
     self.itrs        = itrs
+    self.ms          = Minisat()
+    self.t7          = T7_sat()
+
+    print 'MAX PURES = ' + str(self.max_pures)
 
   def gen_rand_input(self):
     """
@@ -117,6 +58,7 @@ class RandomTesting:
     """
     self.nbvars    = random.randint(1, self.max_vars)
     self.nbclauses = random.randint(1, self.max_clauses)
+    pures_found    = 0
 
     with open(self.path, 'wb') as f:
       f.write('p cnf ' + str(self.nbvars) + ' ' + str(self.nbclauses) + ' \n')
@@ -126,7 +68,7 @@ class RandomTesting:
         used_vars = []
         attempts  = 0
         for j in range(0, random.randint(1, self.nbvars)):
-          if attempts > 1000:
+          if attempts > 10000:
             # do waste time trying to generate random values if duplicates keep occuring
             continue
           var = random.randint(-self.nbvars, self.nbvars)
@@ -135,7 +77,12 @@ class RandomTesting:
             continue 
           elif var in used_vars:
             # do not write duplicate variables (per clause) to file
-            continue 
+            continue
+          elif (abs(var) in used_vars) or (var * -1 in used_vars):
+            # do not allow the clause to become pure if there many pure clauses exist
+            pures_found += 1
+            if pures_found >= self.max_pures:
+              continue
           else:
             used_vars.append(var)
             f.write(str(var) + ' ')
@@ -148,23 +95,25 @@ class RandomTesting:
     """
     Executes the tests. Returns the number of passed tests.
     """
-    log    = Logger(LOG_PATH, True)
-    passed = 0
+    pass_log = Logger(LOG_PASS, False)
+    fail_log = Logger(LOG_FAIL, True)
+    passed   = 0
+
+    pass_log.write('# \tV \tC \tTime \n')
 
     for i in range(0, self.itrs):
       self.gen_rand_input()
       prev_pass  = passed
-      start_time = time.time()
       passed    += self.test()
-      end_time   = time.time()
       if prev_pass == passed:
         input_file  = None
         with open(self.path, 'r') as f:
           input_file = f.read()
-        log.write('Test FAILED with input:\n' + input_file + '\n  run time = ' + str(end_time - start_time))
+        fail_log.write('Test FAILED with input:\n' + input_file)
       else:
-        log.write('Test number [' + str(i) + '] PASSED.\n  nbvars    = ' + str(self.nbvars) + '\n  nbclauses = ' + str(self.nbclauses) + '\n  run time  = ' + str(end_time - start_time))
-    log.close()
+        pass_log.write(str(i) + '\t' + str(self.nbvars) + '\t' + str(self.nbclauses) + '\t' + self.t7.get_cpu_time() + '\n')
+    pass_log.close()
+    fail_log.close()
     return passed
 
   def test(self):
@@ -172,29 +121,17 @@ class RandomTesting:
     Represents an individual test. Compares the output of
     minisat to sat_solver.o.
     """
-    ms = Minisat()
-    ms.run(self.path)
-    t7 = T7_sat()
-    t7.run(self.path)
-    
-    if ms.get_result() == t7.get_result():
+    self.ms.run(self.path)
+    self.t7.run(self.path)
+
+    if self.ms.get_result() == self.t7.get_result():
       return 1
     else:
       return 0
 
-    # ms_out    = Minisat(self.path).run().split('\n')
-    # ms_result = ms_out[len(ms_out) - 2] + '\n'
-    # t7_out    = Sat(self.path).run()
-    
-    # if ms_result == t7_out:
-    #   return 1
-    # else:
-    #   return 0
-
-
 if __name__ == '__main__':
   init_random()
-  passed = RandomTesting(INPUT_PATH, MAX_VARS, MAX_CLAUSES, TEST_RUNS).run()
+  passed = RandomTesting(INPUT_PATH, MAX_VARS, MAX_CLAUSES, MAX_PURES, TEST_RUNS).run()
 
   print '\n\n\nDifferential Testing:'
   print '  PASSED: ' + str(passed)
